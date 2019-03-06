0fd008a7348c947950e455de02f532eba97be41f
Gheorghe Luchianov <gluchianov@live.com>
2019-03-06 18:52:24 +0200

    Update start-proxy.sh

--- start-proxy.sh
+++ start-proxy.sh
@@ -1,11 +1,13 @@
+
+executable file 12 lines (8 sloc) 385 Bytes
 set -e
 
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
-if [ ! -d "$DIR/Twisted-18.9.0" ]; then
-	wget "https://files.pythonhosted.org/packages/5d/0e/a72d85a55761c2c3ff1cb968143a2fd5f360220779ed90e0fadf4106d4f2/Twisted-18.9.0.tar.bz2" -P "$DIR"
-	tar xfv "$DIR/Twisted-18.9.0.tar.bz2" -C "$DIR"
-	rm -f "$DIR/Twisted-18.9.0.tar.bz2"
+if [ ! -d "$DIR/Twisted-13.2.0" ]; then
+	wget "https://pypi.python.org/packages/source/T/Twisted/Twisted-13.2.0.tar.bz2#md5=83fe6c0c911cc1602dbffb036be0ba79" -P "$DIR"
+	tar xfv "$DIR/Twisted-13.2.0.tar.bz2" -C "$DIR"
+	rm -f "$DIR/Twisted-13.2.0.tar.bz2"
 fi
 
-PYTHONPATH="$DIR/Twisted-18.9.0/:$PYTHONPATH" python "$DIR/hlsproxy.py" "$@"
+PYTHONPATH="$DIR/Twisted-13.2.0/:$PYTHONPATH" python "$DIR/hlsproxy.py" "$@"
